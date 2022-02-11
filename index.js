35f1ae824f9d1cc31c8ed98a697cfec7b32ea4de
Voigt <guilherme.voigt@outlook.com>
2022-02-10 23:26:25 -0300

    Conexão com o banco de dados em nuvem, arquivo com informações de validação ocultado

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //configuração inicial
 const express = require('express');
 const app = express(); //executa express como uma função
+const mongoose = require('mongoose');
 
 //forma de ler JSON / middlewares
 app.use(
@@ -19,5 +20,18 @@ app.get('/', (req, res) => {
 })
 
 //entregar uma porta
+const logins = require("./logins");
+const DBUSER = logins.DBUSER;
+const DBPASS = logins.DBPASS;
+    
+const uri = `mongodb+srv://${DBUSER}:${DBPASS}@cluster0.g3y4t.mongodb.net/APIDB?retryWrites=true&w=majority`
 const port = 3000
-app.listen(port)
+mongoose.connect(uri)
+.then( 
+    () => {
+        app.listen(port)
+        console.log("Conectado ao banco de dados. Aplicação iniciada na porta " + port)
+    })
+.catch( (err) => { console.error(err) })
+
+
